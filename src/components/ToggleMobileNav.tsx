793200848bc772364980bc5851a2b3bbdc35e13e
kevin-d <kevin.doublet02@gmail.com>
2023-10-21 16:51:32 +0200

    fix mobile nav css 3.0

--- src/components/ToggleMobileNav.tsx
+++ src/components/ToggleMobileNav.tsx
@@ -6,14 +6,14 @@ type Props = {
 }
 const ToggleMobileNav: FunctionComponent<Props> = ({isOpen}) => {
     return (
-        <div className="relative">
+        <div className="relative h-6 w-6">
             <StaticImage src="../images/icons/close.svg"
                          alt="Icon close navigation"
-                         className={`md:hidden h-6 w-6 transition ease-in-out delay-10 absolute inset-0 ${isOpen ? "flex scale-100" : "scale-0"}`}
+                         className={`md:hidden transition ease-in-out delay-10 absolute inset-0 ${isOpen ? "flex scale-100" : "scale-0"}`}
             />
             <StaticImage src="../images/icons/nav-mobile.svg"
                          alt="Icon mobile navigation"
-                         className={`md:hidden h-6 w-6 transition ease-in-out delay-10 scale-0 ${isOpen ? "scale-0" : "flex scale-100"}`}
+                         className={`md:hidden transition ease-in-out delay-10 absolute inset-0 ${isOpen ? "scale-0" : "flex scale-100"}`}
             />
         </div>
     )
